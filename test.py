f4fd89fa5e9448224fac118772e236a39575ceab
renxq <rxqphper@163.com>
2018-04-26 10:46:33 +0800

    评论爬取第二阶段完成

--- test.py
+++ test.py
@@ -1,36 +1,5 @@
 #!/usr/local/bin/python3
 
-# from selenium import webdriver
-# from bs4 import BeautifulSoup
-#
-# def driver_result_content(url):
-#     driver = webdriver.Firefox()
-#     driver.get(url)
-#     content = driver.page_source
-#     driver.close()
-#     return content
-#
-# def soup_source(html):
-#     return BeautifulSoup(content, 'lxml')
-#
-# home_url = 'http://mobile.zol.com.cn/news/'
-#
-# # 源码
-# content = driver_result_content(home_url)
-#
-# # 解析
-# soup = soup_source(content)
-#
-# # 查找
-# url_list = soup.select('#navswitc-cate a')
-#
-# for url in url_list:
-#     content = driver_result_content(url.get('href'))
-#     soup_nex = soup_source(content)
-#
-
-from mysql import update
-
-sql = '''UPDATE comment SET user_id = 'renxinqiang' WHERE id = 1'''
-
-update.Update().update(sql)
+l = 0
+if l :
+    print(1)
